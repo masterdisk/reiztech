ef400a0839eafdfd7c9c82ad9383efd32fd19567
eduardcostea <costeaeduardnic@gmail.com>
2023-12-20 14:42:20 +0200

    initial

---
+++ src/layouts/dashboard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+  <slot/>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  background: #ff400e;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './assets/styling/style.scss'
+import home from './pages/home.vue'
+
+createApp(home).mount('#app')
